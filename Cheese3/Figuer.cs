aebf90af7b15f2d7d0d2fdd88d81a5e542b0a389
IlyaGavhsin <60525281+IlyaGavhsin@users.noreply.github.com>
2021-06-07 14:43:02 +0300

    rename cs file

--- Cheese3/Figuer.cs
+++ Cheese3/Figuer.cs
@@ -3,44 +3,49 @@
 
 namespace Chess
 {
-    class Program
+    using System;
+    // Гавшин Илья 220 шахматы 3.0 
+
+    namespace Chess
     {
-        static void Main(string[] args)
+        class Program
         {
-            King k = new King(1, 1);
-            Console.WriteLine(k.Move(2, 2));
-            Console.WriteLine(k.Move(1, 2));
-            Console.WriteLine(k.Move(2, 2));
-            Console.WriteLine(k.Move(3, 2));
+            static void Main(string[] args)
+            {
+                King k = new King(1, 1);
+                Console.WriteLine(k.Move(2, 2));
+                Console.WriteLine(k.Move(1, 2));
+                Console.WriteLine(k.Move(2, 2));
+                Console.WriteLine(k.Move(3, 2));
 
-            Queen q = new Queen(2, 2);
-            Console.WriteLine(Environment.NewLine + q.Move(3, 2));
-            Console.WriteLine(q.Move(2, 3));
-            Console.WriteLine(q.Move(5, 5));
-            Console.WriteLine(q.Move(5, 6));
+                Queen q = new Queen(2, 2);
+                Console.WriteLine(Environment.NewLine + q.Move(3, 2));
+                Console.WriteLine(q.Move(2, 3));
+                Console.WriteLine(q.Move(5, 5));
+                Console.WriteLine(q.Move(5, 6));
 
-            Bishop b = new Bishop(2, 2);
-            Console.WriteLine(Environment.NewLine + b.Move(3, 2));
-            Console.WriteLine(b.Move(2, 3));
-            Console.WriteLine(b.Move(5, 5));
-            Console.WriteLine(b.Move(5, 6));
+                Bishop b = new Bishop(2, 2);
+                Console.WriteLine(Environment.NewLine + b.Move(3, 2));
+                Console.WriteLine(b.Move(2, 3));
+                Console.WriteLine(b.Move(5, 5));
+                Console.WriteLine(b.Move(5, 6));
 
-            Rook r = new Rook(2, 2);
-            Console.WriteLine(Environment.NewLine + r.Move(8, 2));
-            Console.WriteLine(r.Move(2, 3));
-            Console.WriteLine(r.Move(5, 5));
-            Console.WriteLine(r.Move(5, 6));
+                Rook r = new Rook(2, 2);
+                Console.WriteLine(Environment.NewLine + r.Move(8, 2));
+                Console.WriteLine(r.Move(2, 3));
+                Console.WriteLine(r.Move(5, 5));
+                Console.WriteLine(r.Move(5, 6));
 
-            Knight n = new Knight(2, 2);
-            Console.WriteLine(Environment.NewLine + n.Move(4, 3));
-            Console.WriteLine(n.Move(2, 3));
-            Console.WriteLine(n.Move(5, 5));
-            Console.WriteLine(n.Move(5, 6));
+                Knight n = new Knight(2, 2);
+                Console.WriteLine(Environment.NewLine + n.Move(4, 3));
+                Console.WriteLine(n.Move(2, 3));
+                Console.WriteLine(n.Move(5, 5));
+                Console.WriteLine(n.Move(5, 6));
 
+            }
         }
     }
-
-    class Figure
+        class Figure
     {
         protected int x;
         protected int y;
